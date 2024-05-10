5ba115ed1804e6724f8d6df6800df9377e203ce6
Thiago Dalla Dea <thiagopdalladea@gmail.com>
2024-05-10 01:53:29 -0300

    salvando urls separadamente

--- futebol/simult/main.py
+++ futebol/simult/main.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from PrettyColorPrinter import add_printer
 from time import sleep, time
 from get_data import get_data
+from urls import get_urls
 
 add_printer(1)
 
@@ -19,78 +20,7 @@ event = threading.Event()
 col_team1 = "bet365_name1"
 col_team2 = "bet365_name2"
 
-urls = [
-    # SERIA A BRASIL
-    [
-        "https://br.betano.com/sport/futebol/brasil/brasileirao-serie-a-betano/10016/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/brasil-33/brasileir%C3%A3o-a-102838",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E88369731/G40/",
-        "https://www.betfair.com/apostas/br/futebol/brasileiro-s%C3%A9rie-a/c-13",
-    ],
-    # SERIE B BRASIL
-    [
-        "https://br.betano.com/sport/futebol/brasil/brasileirao-serie-b/10017/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/brasil-33/brasileir%C3%A3o-b-102361",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E102584281/F0/G40/",
-        "https://www.betfair.com/apostas/br/futebol/brasileiro-s%C3%A9rie-b/c-321319",
-    ],
-    # COPA DO BRASIL
-    # [
-    #     # "https://br.betano.com/sport/futebol/competicoes/brasil/10004/?sl=10008",
-    #     # "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/brasil-33/copa-do-brasil-102723",
-    #     # "https://www.bet365.com/#/AC/B1/C1/D1002/E103295038/F0/G40/",
-    #     # "https://www.betfair.com/apostas/br/futebol/copa-do-brasil/c-89219",
-    # ],
-    # ENGLAND
-    [
-        "https://br.betano.com/sport/futebol/league/premier-league-inglaterra/1/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/inglaterra-14/premier-league-102841",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E91422157/G40/",
-        "https://www.betfair.com/apostas/br/futebol/inglaterra-premier-league/c-10932509",
-    ],
-    # SPAIN
-    [
-        "https://br.betano.com/sport/futebol/espanha/laliga/5/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/espanha-28/laliga-102829",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E91721703/G40/",
-        "https://www.betfair.com/apostas/br/futebol/espanha-la-liga/c-117",
-    ],
-    # GERMANY
-    [
-        "https://br.betano.com/sport/futebol/alemanha/bundesliga/216/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/alemanha-17/bundesliga-102842",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E92193314/G40/",
-        "https://www.betfair.com/apostas/br/futebol/bundesliga-alem%C3%A3/c-59",
-    ],
-    # ITALY
-    [
-        "https://br.betano.com/sport/futebol/italia/serie-a/1635/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/it%C3%A1lia-20/s%C3%A9rie-a-102846",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E92269709/G40/",
-        "https://www.betfair.com/apostas/br/futebol/it%C3%A1lia-s%C3%A9rie-a/c-81",
-    ],
-    # FRANCE
-    [
-        "https://br.betano.com/sport/futebol/franca/ligue-1/215/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/fran%C3%A7a-16/ligue-1-102843",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E92193218/G40/",
-        "https://www.betfair.com/apostas/br/futebol/fran%C3%A7a-ligue-1/c-55",
-    ],
-    # USA
-    [
-        "https://br.betano.com/sport/futebol/eua/mls/17103/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/am%C3%A9rica-do-norte-9/major-league-soccer-102849?tab=matches",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E99351127/F0/G40/",
-        "https://www.betfair.com/apostas/br/futebol/mls/c-141",
-    ],
-    # DENMARK
-    [
-        "https://br.betano.com/sport/futebol/competicoes/dinamarca/11339/",
-        "https://sports.sportingbet.com/pt-br/sports/futebol-4/aposta/dinamarca-13",
-        "https://www.bet365.com/#/AC/B1/C1/D1002/E99511680/F0/G40/",
-        "https://www.betfair.com/apostas/br/futebol/dinamarca-superliga/c-23",
-    ],
-]
+urls = get_urls()
 
 def open_site(name, url, query="*"):
     drivers[name].get(url)
